3515a897c5c97312e044a5c81d0c525746aba300
Mahmoud Khaled <1901654@eng.asu.edu.eg>
2022-05-18 12:18:13 +0200

    add some function to option file

--- Option_Functions.c
+++ Option_Functions.c
@@ -56,3 +56,42 @@ unsigned char Button_C_Pushed (void)
 		goto Check;
 	}
 }
+while(KEYPAD_u8READ(dakhalo hena port_Outname,port_inname ==0xFF)
+{}
+unsigned char input=KEYPAD_u8READ;
+if(input=="D")
+{
+
+clear lcd function ;
+print lcd function ("cooking timee") ;
+
+unsigned char seconds_small=0,seconds_big=0,minutes_small=0,minutes_big =0 ;
+unsigned char i=0;
+for(i=0;i<4;i++)
+{
+while(KEYPAD_u8READ(dakhalo hena port_Outname,port_inname ==0xFF)
+{}
+minutes_big=minutes_small;
+minutes_small=seconds_big;
+seconds_big=seconds_small;
+seconds_small=KEYPAD_u8READ((dakhalo hena port_Outname,port_inname);
+use lcd print function to print minutes_big minutes_small : seconds_big seconds_small
+while(KEYPAD_u8READ(dakhalo hena port_Outname,port_inname ==0xFF)
+{
+if(SW1==0)
+{LCD FUNCTION TO CLEAR
+	i=0
+}
+if(SW2==0)
+{
+i=4
+break;
+
+}
+
+
+
+}
+
+
+}
