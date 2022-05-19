79c69af79c532dbb272ef8a55cea687b873c1cc5
MohamedAbdallah-eng <105590767+MohamedAbdallah-eng@users.noreply.github.com>
2022-05-19 10:55:52 +0200

    added functions to Neeeded_Functions

--- Needed_Functions.c
+++ Needed_Functions.c
@@ -1,3 +1,12 @@
+#include "LCDH.h"
+#include "TimerH.h"
+#include "KeyPadH.h"
+#include "Needed_FunctionsH.h"
+#include "ButtonH.h"
+#include "LEDH.h"
+#include "BuzzerH.h"
+#include "Option_FunctionsH.h"
+
 void Display_Start(void)
 {
 		LCD_Cmd(0x01);
@@ -25,6 +34,14 @@ void LCD_error_msg3(void)
 	LCD_Cmd(0x01);
 	LCD_Cmd(0x80);
 	LCD_Write_String("Please enter a valid number");
+}
+
+void LCD_error_msg4(void)
+{
+	LCD_Cmd(0x01);
+	LCD_Cmd(0x80);
+	LCD_Write_String("Please choose a valid option");
+	check_SW3('D',2);
 }
 
 void pause_display( unsigned char remaining_time ) 
@@ -106,17 +123,42 @@ void Valid_time(unsigned char minutes,unsigned char seconds)
 	else
 	{}
 }
-void LCD_error_msg4(void)
-{
-	LCD_Cmd(0x01);
-	LCD_Cmd(0x80);
-	LCD_Write_String("Please choose a valid option");
-	check_SW3('D',2);
-}
+
 unsigned char Valid_Input(unsigned char Input)  // Checks if the input given is valid or not
 {
 		if (0<Input && Input<10)
 			return 1;
 		else
 			return 0;
+}
+
+void Valid_Input2(unsigned char Input)
+{
+		if(Input<10)
+		{}
+			else
+			{
+				LCD_error_msg3();
+			}
+}
+
+void Count_Down(unsigned char time)
+{
+	int i;
+	for(i=time;i>=0;i--)
+	{
+		LCD_Cmd(0xC0);
+		LCD_Write_Char(i);			/*check LCD if the new number overwrites the previous one*/
+		/*LCD_Write_Char('s');*/
+		TIMER_SEC2(1);
+		if(BUTTON_u8READ('D',2)==0)  /*sw3*/
+		{
+			check_SW3('D',2);
+			Continue_cooking(i);
+		}
+		if(check_SWITCHES('F',4) == 1) /*sw1*/
+		{
+			pause_display(i);
+		}
+  }
 }
