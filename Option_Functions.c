6b29ee272c5e8acf047c241c6dd6e7ed9f7cc290
unknown <1900238@eng.asu.edu.eg>
2022-05-24 01:13:31 +0200

    Add in option function

--- Option_Functions.c
+++ Option_Functions.c
@@ -126,4 +126,17 @@ void Microwave_States(void)
 				Cooking(minutes,seconds);
 				break;
 			}
+			case 'C':
+			{
+				unsigned char Defrosted_Time;
+				unsigned char minutes;
+				unsigned char seconds;
+				Defrosted_Time=Button_C_Pushed();
+				minutes = Defrosted_Time/60;
+				seconds = Defrosted_Time%60;
+				while(BUTTON_u8READ('D',1)==0) {}  // wait until SW2 is pressed to start cooking
+				TIMER_MS(200);
+				Cooking(minutes,seconds);
+				break;
+			}
 			
