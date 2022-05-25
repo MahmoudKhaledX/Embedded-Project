bde185174eec791e16c31bce1ad8ec39b54e5d96
MohamedAbdallah-eng <105590767+MohamedAbdallah-eng@users.noreply.github.com>
2022-05-25 20:34:42 +0200

    latest modified files

--- Needed_Functions.c
+++ Needed_Functions.c
@@ -7,43 +7,36 @@
 #include "BuzzerH.h"
 #include "Option_FunctionsH.h"
 
-void Display_Start(void)
-{
-		LCD_Cmd(0x01);
-		LCD_Cmd(0x80);
-    LCD_Write_String("Please choose");
-	  LCD_Cmd(0xC0);
-	  LCD_Write_String("A,B,C or D");
-}
-
-void LCD_error_msg(void)
+void LCD_error_msg(void)					// Error msg occurs when the input weight from the is not equal to a number from 1 to 9
 {
 	LCD_Cmd(0x01);
-	LCD_Cmd(0x80);
+	LCD_Cmd(0x87);
 	LCD_Write_String("Err");
-	TIMER_SEC(2);				/*no check here*/
+	TIMER_SEC(2);
 }
 
-void LCD_error_msg2(void)
+void LCD_error_msg2(void)				 // Error msg occurs when the user enters an invalid time
 {
 	LCD_Cmd(0x01);
 	LCD_Cmd(0x80);
 	LCD_Write_String("Please enter");
 	LCD_Cmd(0xC0);
 	LCD_Write_String("a valid time");
-	
+	TIMER_SEC(2);
+	Button_D_Pushed();
 }
 
-void LCD_error_msg3(void)
+void LCD_error_msg3(void)				// Error msg occurs when the user enters an invalid char as a time
 {
 	LCD_Cmd(0x01);
 	LCD_Cmd(0x80);
 	LCD_Write_String("Please enter");
 	LCD_Cmd(0xC0);
 	LCD_Write_String("a valid number");
+	TIMER_SEC(2);
 }
 
-void LCD_error_msg4(void)
+void LCD_error_msg4(void)				// Error msg occurs when the user choose anything rather than A,B,C or D
 {
 	LCD_Cmd(0x01);
 	LCD_Cmd(0x80);
@@ -53,81 +46,38 @@ void LCD_error_msg4(void)
 	check_SW3('D',2);
 }
 
-void pause_display( unsigned char minutes, unsigned char seconds ) /*use it if sw1 is pressed one time*/
-{  
-   LCD_Cmd(0x01);
-   LCD_Cmd(0x80);
-   LCD_Write_String("Paused!");
-   LCD_Cmd(0xC5);
-   LCD_Write_String("left");
-   LCD_WRITE_MINUTES_SECONDS(minutes,seconds);		
-	 while((BUTTON_u8READ('D',2)==0) && (check_SWITCHES('F',4)==0)) {}
-	 if(check_SWITCHES('F',4)==0)  /*sw1*/
+unsigned char Valid_Input(unsigned char Input)  // Checks if the weight given is valid or not
+{
+		if ((48<Input) && (Input<58))
+			return 1;
+		else
+			return 0;
+}
+
+unsigned char Valid_Input2(unsigned char Input)	// Checks if the time given is a number or not
+{
+		if((48<=Input) && (Input<58))
 		{
-			Stopcooking_dispaly();
+			return 1;
 		}
-		if(check_SWITCHES('F',0) == 1) /*sw2*/
-		{
-			Continue_cooking(minutes,seconds);
-		}
-   /*LCD_Write_Char('s')*/
+			else
+			{
+				return 0;
+			}
 }
 
-void Stopcooking_dispaly (void)	
+void Display_Start(void)													
 {
-   LCD_Cmd(0x01); 
-   LCD_Cmd(0x80); 
-   LCD_Write_String("Cooking");
-   LCD_Cmd(0xc0);
-   LCD_Write_String("stopped!");
-	 TIMER_SEC(1);
-	 Microwave_States();
+		LCD_Cmd(0x01);
+		LCD_Cmd(0x80);
+	  LCD_Write_String("Please choose");
+	  LCD_Cmd(0xC0);
+	  LCD_Write_String("A,B,C or D");
 }
 
-unsigned char Time_to_Sec(unsigned char minutes_big,unsigned char minutes_small,unsigned char seconds_big,unsigned char seconds_small)
+void Valid_time(unsigned char minutes,unsigned char seconds) // Checks if the time given is valid or not
 {
-	unsigned char time_sec;
-	unsigned char minutes=minutes_big*10+minutes_small;
-	unsigned char seconds=seconds_big*10+seconds_small;
-	Valid_time(minutes,seconds);
-	time_sec = 60*minutes+seconds;
-	return time_sec;
-	//if choose (D) enter Total time in seconds in function Start Cooking
-}
-
-void check_SW3(unsigned char portname,unsigned char pinumber)
-	/* check sw3 at the beginning, Commands may be deleted if you use it in the main as a while loop condition	*/
-{	
-	if(BUTTON_u8READ('D',2)==0)
-	{
-	LCD_Cmd(0x01);
-	LCD_Cmd(0x80);
-	LCD_Write_String("Close the Door");
-	while ( BUTTON_u8READ('D',2) == 0)
-		{}
-	}
-}
-	
-unsigned char check_SWITCHES (unsigned char portname,unsigned char pinumber)  //check sw1,sw2 
-{ 
-	unsigned char x;
-  x = BUTTON_u8READ(portname,pinumber);    
-  if(x == 1)
-{
-   return 0;
-}
-else
-   return 1;
-}
-void LCD_Write_Weight(unsigned char weight)   //Writes the given weight on the LCD screen
-{
-	LCD_Write_Char(weight);
-	LCD_Write_String("Kg");
-	TIMER_SEC(2);
-}
-void Valid_time(unsigned char minutes,unsigned char seconds)
-{
-	if(minutes>30 || (minutes == 0 && seconds == 0))
+	if(minutes>30 || ((minutes == 0) && (seconds == 0)))
 	{
 		LCD_error_msg2();
 	}
@@ -144,67 +94,7 @@ void Valid_time(unsigned char minutes,unsigned char seconds)
 	{}
 }
 
-unsigned char Valid_Input(unsigned char Input)  // Checks if the input given is valid or not
-{
-		if ((48<Input) && (Input<58))
-			return 1;
-		else
-			return 0;
-}
-
-void Valid_Input2(unsigned char Input)
-{
-		if(Input<10)
-		{}
-			else
-			{
-				LCD_error_msg3();
-			}
-}
-
-void Cooking( unsigned char minutes, unsigned char seconds )
-{
-	check_SW3('D',2);
-	TIMER_MS(200);
-	LCD_Cmd(0x01);
-	LCD_Cmd(0x80);
-	LCD_Write_String("Cooking...");
-	Count_Down(minutes,seconds);
-	
-}
-
-void Count_Down(unsigned char minutes, unsigned char seconds)
-{
-unsigned char i,temp,j ;
-	for (i=minutes;i>=0;i--)
-	{
-				if(i !=minutes)
-				{
-					temp=59;
-				}
-				else
-				{		
-				temp=seconds;
-				}
-
-					for(j=temp;j>=0;j--)
-					{
-						LCD_WRITE_MINUTES_SECONDS(i,j);
-						TIMER_SEC2(1);
-						if(BUTTON_u8READ('D',2)==0)  
-						{
-						check_SW3('D',2);
-						Continue_cooking(i,j);
-						}
-						if(check_SWITCHES('F',4) == 1) 
-						{
-						pause_display(i,j);
-						}
-					}
-	}
-}
-
-void Flash_LEDs_Buzzer(unsigned char Number_Flashes)
+void Flash_LEDs_Buzzer(unsigned char Number_Flashes)				// Blinks LEDs and Buzzer
 {
 	unsigned char i;
 	for (i = Number_Flashes;i>0;i--)
@@ -221,47 +111,113 @@ void Flash_LEDs_Buzzer(unsigned char Number_Flashes)
 		TIMER_MS(500);
 	}
 }
-
-
-
-void LCD_WRITE_MINUTES_SECONDS(unsigned char minutes,unsigned char seconds) // use this to print minutes and seconds on the lcd
+void LCD_Write_Weight(unsigned char weight)  // Writes the given weight on the LCD screen
 {
-		unsigned char minutes_big,minutes_small,seconds_big,seconds_small;
-
-		minutes_big=(minutes/10);
-		minutes_small=(minutes%10);
-		seconds_big=(seconds/10);
-		seconds_small=(seconds%10);
-		LCD_Cmd(0xC5);
-		LCD_Write_Number(minutes_big);
-		LCD_Write_Number(minutes_small);
-	  LCD_Write_String(":");
-		LCD_Write_Number(seconds_big);
-		LCD_Write_Number(seconds_small);
+	LCD_Write_String("Weight = ");
+	LCD_Write_Char(weight);
+	LCD_Write_String(" Kg");
+	TIMER_SEC(2);
 }
 
-void Display_LCD_D(unsigned char seconds_small, unsigned char seconds_big, unsigned char minutes_small, unsigned char minutes_big)
+unsigned char Time_to_Sec(unsigned char minutes_big,unsigned char minutes_small,unsigned char seconds_big,unsigned char seconds_small)
 {
+	unsigned char time_sec;
+	unsigned char minutes=(minutes_big-48)*10+(minutes_small-48);
+	unsigned char seconds=(seconds_big-48)*10+(seconds_small-48);
+	Valid_time(minutes,seconds);
+	time_sec = 60*minutes+seconds;
+	return time_sec;
+	//if choose (D) enter Total time in seconds in function Start Cooking
+}
+
+void Cooking( unsigned char minutes, unsigned char seconds )
+{
+	check_SW3('D',2);
+	TIMER_MS(200);
 	LCD_Cmd(0x01);
-	LCD_Cmd(0x85);
-	LCD_Write_Number(minutes_big);
-	LCD_Write_Number(minutes_small);
-	LCD_Write_String(" :");
-	LCD_Write_Number(seconds_big);
-	LCD_Write_Number(seconds_small);
+	LCD_Cmd(0x80);
+	LCD_Write_String("Cooking...");
+	Count_Down(minutes,seconds);
+	
 }
 
-void Continue_cooking( unsigned char minutes, unsigned char seconds )
+unsigned char check_SWITCHES (unsigned char portname,unsigned char pinumber)
 { 
-     LCD_Cmd(0x01);
-     LCD_Cmd(0x80);
-     LCD_Write_String("Cooking...");
-		 LCD_Cmd(0xc5);
-     LCD_Write_String("left");
-     Count_Down(minutes,seconds);
-     Finish_Cooking();
+	unsigned char x;
+  x = BUTTON_u8READ(portname,pinumber);    // read the value of SW1
+  if(x == 1)
+  {
+   return 0;
+  }
+  else
+   return 1;
 }
-void Finish_Cooking(void)	 //Indicates that the cooking operation has finished
+
+void check_SW3(unsigned char portname,unsigned char pinumber)
+{	
+	if(BUTTON_u8READ('D',2)==0)
+	{
+	LCD_Cmd(0x01);
+	LCD_Cmd(0x80);
+	LCD_Write_String("Close the Door!");
+	while ( BUTTON_u8READ('D',2) == 0)
+		{}
+	}
+}
+
+unsigned char Display_LCD_D(unsigned char seconds_small, unsigned char seconds_big, unsigned char minutes_small, unsigned char minutes_big)
+	// Prints the time given by the user on the LCD
+{
+	unsigned char K;
+	K = Valid_Input2(seconds_small);
+	if (K == 0)
+	{
+		LCD_error_msg3();
+		return 1;
+	}
+	LCD_Cmd(0xC5);
+	LCD_Write_Char(minutes_big);
+	LCD_Write_Char(minutes_small);
+	LCD_Write_String(":");
+	LCD_Write_Char(seconds_big);
+	LCD_Write_Char(seconds_small);
+	return 0;
+}
+
+void pause_display( unsigned char minutes, unsigned char seconds ) // pauses cooking if SW1 is pressed once
+{  
+   LCD_Cmd(0x01);
+   LCD_Cmd(0x80);
+   LCD_Write_String("Paused!");
+   LCD_Cmd(0xC5);
+   LCD_Write_String("left");
+   LCD_WRITE_MINUTES_SECONDS(minutes,seconds);
+	 TIMER_MS(500);
+	 while((BUTTON_u8READ('D',1)==0) && (check_SWITCHES('F',4)==0)) {}
+	 if(check_SWITCHES('F',4)==1)  /*sw1*/
+		{
+			Stopcooking_dispaly();
+		}
+		if(BUTTON_u8READ('D',1) == 1) /*sw2*/
+		{
+			TIMER_MS(200);
+			Continue_cooking(minutes,seconds);
+		}
+   /*LCD_Write_Char('s')*/
+}
+
+void Stopcooking_dispaly ()											// Stops the cooking operation if SW1 is pressed twice
+{
+   LCD_Cmd(0x01);
+   LCD_Cmd(0x80);
+   LCD_Write_String("Cooking");
+   LCD_Cmd(0xc0);
+   LCD_Write_String("stopped!");
+	 TIMER_SEC(3);
+	 Microwave_States();
+}
+
+void Finish_Cooking(void)												// Indicates that the cooking operation has finished
 {
 	LCD_Cmd(0x01);
 	LCD_Cmd(0x80);
@@ -271,4 +227,68 @@ void Finish_Cooking(void)	 //Indicates that the cooking operation has finished
 	Flash_LEDs_Buzzer(3);
 }
 
+void Continue_cooking( unsigned char minutes, unsigned char seconds )			// Continue cooking from the time it was paused at
+{ 
+     LCD_Cmd(0x01);
+     LCD_Cmd(0x80);
+     LCD_Write_String("Cooking...");
+     Count_Down(minutes,seconds);
+     Finish_Cooking();
+		 Microwave_States();
+}
 
+void Count_Down(unsigned char minutes, unsigned char seconds)					// Counts down the remaining cooking time
+{
+unsigned char i,temp,j ;
+	for (i=minutes;i>=0;i--)
+	{
+				if(i !=minutes)
+				{
+					temp=59;
+				}
+				else
+				{		
+				temp=seconds;
+				}
+
+					for(j=temp;j>=0;j--)
+					{
+						LCD_WRITE_MINUTES_SECONDS(i,j);										// writes minutes and seconds on the LCD
+						TIMER_SEC2(1);																		// delay 1 sec and checks on SW1 & SW3
+						if(BUTTON_u8READ('D',2)==0)
+						{
+						check_SW3('D',2);
+						Continue_cooking(i,j);
+						}
+						if(check_SWITCHES('F',4) == 1)
+						{
+						pause_display(i,j);
+						}
+						if(j==0)
+						{
+							break;
+						}
+						
+					}
+					if(i==0)
+						{
+							break;
+						}
+	}
+}
+
+void LCD_WRITE_MINUTES_SECONDS(unsigned char minutes,unsigned char seconds) 		// Prints minutes and seconds on the LCD
+{
+		unsigned char minutes_big,minutes_small,seconds_big,seconds_small;
+
+		minutes_big=(minutes/10);															// minutes_big equals the most significant bit of the minuts
+		minutes_small=(minutes%10);													 // minutes_small equals the least significant bit of the minuts
+		seconds_big=(seconds/10);														// seconds_big equals the most significant bit of the seconds
+		seconds_small=(seconds%10);												 // seconds_small equals the least significant bit of the seconds
+		LCD_Cmd(0xC5);
+		LCD_Write_Number(minutes_big);
+		LCD_Write_Number(minutes_small);
+	  LCD_Write_String(":");
+		LCD_Write_Number(seconds_big);
+		LCD_Write_Number(seconds_small);
+}
--- Option_Functions.c
+++ Option_Functions.c
@@ -5,32 +5,31 @@
 #include "ButtonH.h"
 #include "Option_FunctionsH.h"
 
-
 void Button_A_Pushed(void)
 {
   LCD_Cmd( 0x01 ); 									 // clears LCD display
-  LCD_Cmd( 0x80 );  								// Takes cursor to beginning of first row 
+  LCD_Cmd( 0x84 );  								// Takes cursor to beginning of first row 
 	LCD_Write_String ("Popcorn");			// display popcorn on the screen
 	TIMER_SEC(2);  										// delay for 2 seconds
 }
 
-unsigned char Button_B_Pushed (void)
+unsigned long Button_B_Pushed (void)
 {
 	unsigned char Kilos;
 	unsigned char Check_Result;
-	unsigned char Defrosted_Time;
+	unsigned long Defrosted_Time;
 	Check:
 	LCD_Cmd(0x01);												// Clears the LCD display
-	LCD_Cmd(0x80);												// Takes cursor to beginning of first row
+	LCD_Cmd(0x82);												// Takes cursor to beginning of first row
 	LCD_Write_String("Beef Weight?");
-	TIMER_SEC(2);            
+	TIMER_SEC(1);
 	Kilos = KEYPAD_u8READ( 'C','E'); 			// gets the weight from the keypad
 	Check_Result = Valid_Input(Kilos);		// checks if the input weight is a valid number
 	if (Check_Result == 1)
 	{
 		LCD_Cmd(0x01);
 		LCD_Write_Weight(Kilos);						// prints the input weight on the LCD
-		Defrosted_Time = Kilos * 0.5 * 60;
+		Defrosted_Time = (Kilos-48) * 0.5 * 60;
 		return Defrosted_Time;
 	}
 	else
@@ -45,17 +44,18 @@ unsigned char Button_C_Pushed (void)
 	unsigned char Kilos;
 	unsigned char Check_Result;
 	unsigned char Defrosted_Time;
+	Check:
 	LCD_Cmd(0x01);												// Clears the LCD display
 	LCD_Cmd(0x80);												// Takes cursor to beginning of first row
 	LCD_Write_String("Chicken Weight?");
-	Check:
+	TIMER_SEC(1);            
 	Kilos = KEYPAD_u8READ( 'C','E'); 			// gets the weight from the keypad
 	Check_Result = Valid_Input(Kilos);		// checks if the input weight is a valid number
 	if (Check_Result == 1)
 	{
 		LCD_Cmd(0x01);
 		LCD_Write_Weight(Kilos);						// prints the input weight on the LCD
-		Defrosted_Time = Kilos * 0.2 * 60;
+		Defrosted_Time = (Kilos-48) * 0.2 * 60;
 		return Defrosted_Time;
 	}
 	else
@@ -64,43 +64,57 @@ unsigned char Button_C_Pushed (void)
 		goto Check;
 	}
 }
+		
 unsigned char Button_D_Pushed()
 {
+	loop:
 	LCD_Cmd(0x01);
-	LCD_Cmd(0x80);
-  LCD_Write_String("cooking time?") ;
-  unsigned char seconds_small=0;
-	unsigned char seconds_big=0; 
-	unsigned char minutes_small=0;
-	unsigned char minutes_big =0 ;
+	LCD_Cmd(0x81);
+  LCD_Write_String("Cooking time?") ;
+  unsigned char seconds_small=48;
+	unsigned char seconds_big=48; 
+	unsigned char minutes_small=48;
+	unsigned char minutes_big =48 ;
   unsigned char i;
-	LCD_Cmd(0x85);
+	unsigned char K;
+	LCD_Cmd(0xC5);
 	LCD_Write_String("00:00");
   for(i=0;i<4;i++)
 	{
-	 TIMER_MS(700);
 	 minutes_big=minutes_small;
    minutes_small=seconds_big;
    seconds_big=seconds_small;
    seconds_small=KEYPAD_u8READ('C','E');
-	 Display_LCD_D(seconds_small,seconds_big,minutes_small,minutes_big);
-while(KEYPAD_u8READ2('C','E') ==0xFF)
-{
-if(check_SWITCHES('F',4)==1)
-	LCD_Cmd(0x01);
-	i=0;
-}
-if(check_SWITCHES('F',0)==1)
-{
-i=4;
-break;
-}
-}
+	 K = Display_LCD_D(seconds_small,seconds_big,minutes_small,minutes_big);
+	 if(K == 1)
+	 {
+		 goto loop;																		// starts from the beginning if the user entered an invalid char as a number like '*'
+	 }
+	 TIMER_SEC(1);
+   while(KEYPAD_u8READ2('C','E') ==0xFF)					// while no Key is entered at the keypad check switches
+   {
+    if(check_SWITCHES('F',4)==1)									// Checks SW1
+		{
+	   LCD_Cmd(0x01);
+		 goto loop;
+		}
+    if(BUTTON_u8READ('D',1)==1)										// Checks SW3
+    {
+     TIMER_MS(200);
+		 unsigned char Total_Time=Time_to_Sec(minutes_big,minutes_small,seconds_big,seconds_small);
+		 unsigned char minutes = Total_Time/60;
+		 unsigned char seconds = Total_Time%60;
+	   Cooking(minutes,seconds);
+		 Finish_Cooking();
+		 Microwave_States();
+    }
+	 }
+  }
 	unsigned char Total_Time;
 	Total_Time=Time_to_Sec(minutes_big,minutes_small,seconds_big,seconds_small);
 	return(Total_Time);
 }
-
+		
 void Microwave_States(void)
 {
 		unsigned char Keypad_Input;
@@ -119,7 +133,7 @@ void Microwave_States(void)
 			}
 		  case 'B':
 			{
-				unsigned char Defrosted_Time;
+				unsigned long Defrosted_Time;
 				unsigned char minutes;
 				unsigned char seconds;
 				Defrosted_Time=Button_B_Pushed();
@@ -167,4 +181,3 @@ void Microwave_States(void)
 		Finish_Cooking();
 		goto start;
 }
-		
