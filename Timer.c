52d4788d861c20752daecd37ced576fd71a9fa76
MohamedAbdallah-eng <105590767+MohamedAbdallah-eng@users.noreply.github.com>
2022-05-25 02:07:09 +0200

    modified Timer.c and added files

--- Timer.c
+++ Timer.c
@@ -1,4 +1,4 @@
-#include "DPIOH.h"
+#include "TM4C123.h"
 #include "tm4c123gh6pm.h"
 #include "ButtonH.h"
 
@@ -50,7 +50,7 @@ void TIMERCOUNTER_xHZ2(unsigned long delay) // you will never call this function
 	NVIC_ST_RELOAD_R= delay-1;
 	NVIC_ST_CURRENT_R=0;
 	NVIC_ST_CTRL_R=0x5;
-  while ((NVIC_ST_CTRL_R & 0x00010000 )==0 && (BUTTON_u8READ('F',4) == 1) && (BUTTON_u8READ('D',3) == 1));	//sw1 & sw3
+  while ((NVIC_ST_CTRL_R & 0x00010000 )==0 && (BUTTON_u8READ('F',4) == 1) && (BUTTON_u8READ('D',2) == 1));	//sw1 & sw3
 }
 
 void TIMER_MS2(unsigned long times)//call it to wait "X" ms send X as argument with the amount of MS you want to wait
